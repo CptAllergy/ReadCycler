90ea57b34bd3607d829b07018158cbf4146bd502
Pereira234 <56123614+Pereira234@users.noreply.github.com>
2021-12-04 17:01:25 +0000

    corrigidos bugs das modais

--- scripts.js
+++ scripts.js
@@ -16,6 +16,7 @@ var btnLogIn = document.getElementById("logIn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
 btnSignUp.onclick = function() {
@@ -30,20 +31,26 @@ span.onclick = function() {
   modal.style.display = "none";
     modalLogin.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal.style.display = "none";
+    modalLogin.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modalLogin) {
+  if (event.target == modal || event.target == modalLogin) {
+    modal.style.display = "none";
       modalLogin.style.display = "none";
   }
 }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modalLogin) {
+//       modalLogin.style.display = "none";
+//   }
+// }
 
 
 function includeHTML() {
